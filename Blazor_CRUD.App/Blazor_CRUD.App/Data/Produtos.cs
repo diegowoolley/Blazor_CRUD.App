401651447061ec3b71ce594a4ad86c21e9ac6cf7
Diego Woolley <ssmdiego@gmail.com>
2024-12-04 23:56:28 -0300

    aterações do botao mudar foto de clientes e produtos para chamar um componente e nao um evento e criação de um modal para notificações

--- Blazor_CRUD.App/Blazor_CRUD.App/Data/Produtos.cs
+++ Blazor_CRUD.App/Blazor_CRUD.App/Data/Produtos.cs
@@ -12,41 +12,49 @@ namespace BlazorCRUD.App.Data
 
         public int Id { get; set; }
 
-        [MaxLength(20)]
+        [StringLength(20, ErrorMessage ="O código de barras não pode ter mais de 13 carac")]
         public string? Cod_Barras { get; set; }
 
-        [MaxLength(100)]
+        [Required(ErrorMessage ="O campo descrição é obrigatório.")]
+        [StringLength(100, ErrorMessage ="A descrição não pode conter mais de 100 carácteres.")]
         public string Descricao { get; set; } = string.Empty;
 
         [ForeignKey("Categoria_Id_PK")]
         public int Id_categoria { get; set; }
 
+        [Required(ErrorMessage ="O campo unidade de medida é obrigatório.")]
         [MaxLength(10)]
         public string? Un_Medida { get; set; }
 
+        [Required(ErrorMessage ="O campo quantidade é obrigatório")]
+        [Range(1,99999999, ErrorMessage ="A quantidade não pode ser menor que 1.")]
         public int Quantidade { get; set; }
 
         [ForeignKey("Fornecedor_Id_PK")]
         public int Id_fornecedor { get; set; }
 
         [Column(TypeName = "decimal(10,0)")]
-        public Decimal? Peso_Medio { get; set; }
+        public Decimal? Peso_Medio { get; set; } = decimal.Zero;
 
         [Column(TypeName = "decimal(10,2)")]
-        public decimal? Peso_bruto { get; set; }
+        public decimal? Peso_bruto { get; set; } = decimal.Zero;
+
+        [Required(ErrorMessage ="O campo vl.compra é obrigatório.")]
+        [Range(0.01, 99999999999, ErrorMessage = "O valor de compra não pode ser abaixo de 0.01.")]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal? Valor_Compra { get; set; } = decimal.Zero;
 
         [Column(TypeName = "decimal(10,2)")]
-        public decimal? Valor_Compra { get; set; }
+        public decimal? Margem_Lucro { get; set; } = decimal.Zero;
 
+        [Required(ErrorMessage = "O campo vl.venda é obrigatório.")]
+        [Range(0.01, 99999999999, ErrorMessage = "O valor de venda não pode ser abaixo de 0.01.")]
         [Column(TypeName = "decimal(10,2)")]
-        public decimal? Margem_Lucro { get; set; }
+        public decimal Valor_Venda { get; set; } = decimal.Zero;
 
-        [Column(TypeName = "decimal(10,2)")]
-        public decimal Valor_Venda { get; set; }
+        public int? estoque_min { get; set; } = 0;
 
-        public int? estoque_min { get; set; }
-
-        public int? estoque_max { get; set; }
+        public int? estoque_max { get; set; } = 0;
 
         public Byte[]? Foto { get; set; }
 
